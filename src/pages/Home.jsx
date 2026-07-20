d2f6ad92f4cedcd09d55c03025390a8f2b8756ee
mahmoud.wahba.dev <mahmoud.wahba.dev@gmail.com>
2026-07-20 16:43:12 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,24 +45,7 @@ const Home = () => {
               نظرة عامة على طلباتك وإحصائياتك
             </p>
           </div>
-          <Link
-            to="/match-request"
-            className="btn btn-primary font-medium text-14px"
-          >
-            <svg
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M5 6.66667H0V5H5V0H6.66667V5H11.6667V6.66667H6.66667V11.6667H5V6.66667Z"
-                fill="white"
-              />
-            </svg>
-            طلب مطابقة جديد
-          </Link>
+    
         </div>
 
         {loading ? (
